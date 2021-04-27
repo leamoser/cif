2924ed0b16e1661279b2664326860001e690db79
Lea Moser <lea.moser@stud.fhgr.ch>
2021-04-27 11:10:27 +0200

    added and tested axios

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,20 @@
 import { createStore } from 'vuex'
+import axios from "axios"
 
 export default createStore({
   state: {
   },
   mutations: {
+    getCode(state, content){
+      console.log(content)
+    }
   },
   actions: {
+    getCode({ commit }) {
+      axios.get('http://188.166.165.104/items/users?access_token=hallo').then(response => {
+        commit('getCode', response.data.data);
+      })
+    }
   },
   modules: {
   }
