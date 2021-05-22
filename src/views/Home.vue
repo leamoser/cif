2034a11e65f3555939b2a110ab201220f25f1b64
Lea Moser <lea.moser@stud.fhgr.ch>
2021-05-22 13:33:27 +0200

    changed favicons

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -22,7 +22,7 @@ export default {
   },
   data(){
     return{
-      bigText: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.',
+      bigText: 'Programmieren lernen kann auch Spass machen. Mit CIF* spielerisch erfolge feiern und besser werden.',
       title: 'Alle angebotenen Kurse',
       desc: 'Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.'
     }
