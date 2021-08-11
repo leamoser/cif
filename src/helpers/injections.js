84a1250ec517983c977a624205a08775331755bb
Lea Moser <lea.moser@stud.fhgr.ch>
2021-08-11 18:36:24 +0200

    added injections-file

---
+++ src/helpers/injections.js
@@ -0,0 +1,6 @@
+export const injections = {
+    //example
+    sum: (x,y) => {
+        return x + y;
+    }
+}
--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import router from './router'
 import store from './store'
 import axios from "axios";
 import slugify from "slugify-js"
+import { injections } from "./helpers/injections.js"
 
 //styles
 import '/src/css/main.scss';
@@ -14,6 +15,7 @@ import '/src/css/main.scss';
 const myapp = createApp(App);
 myapp.config.globalProperties.$axios = axios;
 myapp.config.globalProperties.$slugify = slugify;
+myapp.config.globalProperties.$injection = injections;
 myapp.use(store);
 myapp.use(router);
 myapp.mount('#app');
